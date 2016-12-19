14e2c44cf822d238ec1c2aa649b01f20004311b7
Felipe Souza Lima <Felipe Souza Lima>
2016-12-18 22:19:49 -0200

    Working with variables.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,3 +2,8 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append("Felipe Souza Lima");
+
+var firstName = "Tim";
+var age = 34;
+
+console.log(firstName);
